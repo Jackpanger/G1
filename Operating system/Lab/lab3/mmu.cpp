d69fdf2baa853339fe509bbffe02ed5d672b09dd
Jack Pang <jp6862@nyu.edu>
2023-04-27 08:44:11 -0400

    lab3

--- Operating system/Lab/lab3/mmu.cpp
+++ Operating system/Lab/lab3/mmu.cpp
@@ -1,10 +1,7 @@
 #include "mmu.h"
 #include <stdio.h>
-#include <iostream>
-#include <ctype.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <cstring>
 
 using namespace std;
 using namespace mmu;
@@ -12,87 +9,272 @@ using namespace mmu;
 pte_t page_table[MAX_VPAGES]; // a per process array of fixed size=64 of pte_t not pte_t pointers !
 Pager *THE_PAGER = nullptr;
 process_t *current_process;
-vector<int> randvals;
-int ofs = 0;    
+unsigned int current_pid;
+Pstats p;
+Pstats *pstats = &p;
+bool free_frame_list_full = false;
 
 
-
-frame_t *allocate_frame_from_free_list()
-{
-
-    return nullptr;
+int allocate_frame_from_free_list() {
+    if (frame_free_list.empty()) {
+        free_frame_list_full = true;
+        return -1;
+    }
+    free_frame_list_full = false;
+    int hand = frame_free_list.front();
+    frame_free_list.pop();
+    return hand;
 }
 
-frame_t *get_frame()
-{
-    frame_t *frame = allocate_frame_from_free_list();
-    if (frame == NULL)
+unsigned int get_frame() {
+    int frame = allocate_frame_from_free_list();
+    if (frame == -1) {
         frame = THE_PAGER->select_victim_frame();
+
+    }
     return frame;
 }
 
-void simulation(FileLoader &exec)
-{
+bool page_fault_handler(pte_t *pte, unsigned int vpage) {
+    for (vma_t vma: current_process->vmas) {
+        if (vpage >= vma.start_vpage && vpage <= vma.end_vpage) {
+            pte->write_protect = vma.write_protect;
+            pte->file_mapped = vma.file_mapped;
+            return true;
+        }
+    }
+    return false;
+}
+
+void unmap_old_frame(frame_t *newframe){
+    unsigned int pid = newframe->proc_id;
+    unsigned int pre_vpage = newframe->vpage;
+    process_t *pre_proc = &process_set.at(pid);
+    pte_t *pre_pte = &pre_proc->page_table[pre_vpage];
+    if (OPTION_O) printf(" UNMAP %d:%d\n", pid, pre_vpage);
+    pre_proc->pstats.unmaps++;
+    cost += cost_map::UNMAPS;
+    pre_pte->present = false;
+    if (pre_pte->modified) {
+        pre_pte->pageout = !pre_pte->file_mapped;
+        if (pre_pte->file_mapped) {
+            if (OPTION_O)
+                printf(" FOUT\n");
+            pre_proc->pstats.fouts++;
+            cost += cost_map::FOUTS;
+        } else {
+            if (OPTION_O)
+                printf(" OUT\n");
+            pre_proc->pstats.outs++;
+            cost += cost_map::OUTS;
+        }
+    }
+}
+void option_operation(pte_t* pte){
+    if (pte->pageout) {
+        if (OPTION_O) printf(" IN\n");
+        current_process->pstats.ins++;
+        cost += cost_map::INS;
+    } else if (pte->file_mapped) {
+        if (OPTION_O) printf(" FIN\n");
+        current_process->pstats.fins++;
+        cost += cost_map::FINS;
+    } else {
+        if (OPTION_O) printf(" ZERO\n");
+        current_process->pstats.zeros++;
+        cost += cost_map::ZEROS;
+    }
+}
+
+void exit_process(unsigned int vpage){
+    for (int i = 0; i < current_process->page_table.size(); i++) {
+        pte_t &pte = current_process->page_table[i];
+        pte.pageout = false;
+        if (pte.present) {
+            pte.present = false;
+            frame_table[pte.frame].mapped = false;
+            frame_free_list.push(pte.frame);
+            if (OPTION_O) printf(" UNMAP %d:%d\n",vpage, i );
+            current_process->pstats.unmaps++;
+            cost += cost_map::UNMAPS;
+            if (pte.file_mapped && pte.modified) {
+                if (OPTION_O) printf(" FOUT\n");
+                current_process->pstats.fouts++;
+                cost += cost_map::FOUTS;
+            }
+        }
+    }
+}
+
+void simulation(FileLoader &exec) {
     char operation;
     unsigned int vpage;
-    while (exec.get_next_instruction(operation, vpage))
-    {
-        // handle special case of “c” and “e” instruction // now the real instructions for read and write
-        pte_t *pte = &current_process->page_table[vpage];
-        if (!pte->present)
-        {
-            // this in reality generates the page fault exception and now you execute
-            // verify this is actually a valid page in a vma if not raise error and next inst
-            frame_t *newframe = get_frame();
-            //-> figure out if/what to do with old frame if it was mapped
-            // see general outline in MM-slides under Lab3 header and writeup below
-            // see whether and how to bring in the content of the access page.
+    int t = 0;
+    while (exec.get_next_instruction(operation, vpage)) {
+        if (OPTION_O) printf("%d: ==> %c %d\n", inst_count++, operation, vpage);
+
+        switch (operation) {
+            case 'c': {
+                current_process = &process_set.at(vpage);
+                current_pid = vpage;
+//                ::printf("current_pid: %u\n",current_pid);
+                ctx_switches++;
+                cost+=cost_map::CONTEXT_SWITCH;
+                break;
+            }
+            case 'w':
+            case 'r': {
+                // handle special case of “c” and “e” instruction // now the real instructions for read and write
+                pte_t *pte = &current_process->page_table[vpage];
+
+                pte->referenced = true;
+                cost+=cost_map::LOAD_STORE;
+                if (!pte->present) {
+                    // this in reality generates the page fault exception and now you execute
+                    // verify this is actually a valid page in a vma if not raise error and next inst
+                    pte->modified=false;
+                    if (page_fault_handler(pte, vpage)) {
+                        unsigned int frame_id = get_frame();
+                        frame_t *newframe = &frame_table[frame_id];
+                        // UNMAP old frame
+                        if (free_frame_list_full){
+                            unmap_old_frame(newframe);
+                        }
+                        // map new frame
+                        newframe->proc_id = current_pid;
+                        newframe->vpage = vpage;
+                        newframe->mapped = true;
+                        pte->present = true;
+                        pte->frame = frame_id;
+                        // print option_O
+                        option_operation(pte);
+                        if (OPTION_O) printf(" MAP %d\n", frame_id);
+                        current_process->pstats.maps++;
+                        cost += cost_map::MAPS;
+                        THE_PAGER->set_age(frame_id);
+                    } else {
+                        if (OPTION_O) printf(" SEGV\n");
+                        current_process->pstats.segv++;
+                        cost += cost_map::SEGV;
+                    }
+                    //-> figure out if/what to do with old frame if it was mapped
+                    // see general outline in MM-slides under Lab3 header and writeup below
+                    // see whether and how to bring in the content of the access page.
+                }
+                if (operation == 'w') {
+                    if (pte->write_protect) {
+                        if (OPTION_O) printf(" SEGPROT\n");
+                        current_process->pstats.segprot++;
+                        cost += cost_map::SEGPROT;
+                    } else {
+                        pte->modified = true;
+                    }
+                }
+                break;
+            }
+            case 'e': {
+//                printf("EXIT current process %d\n",current_pid);
+                process_exits++;
+                cost+= cost_map::EXIT;
+                exit_process(vpage);
+                break;
+            }
+
         }
+
         // now the page is definitely present
         // check write protection
         // simulate instruction execution by hardware by updating the R/M PTE bits update_pte(read/modify) bits based on operations.
     }
-}
 
-int main(int argc, char *const argv[])
-{
+}
+void display(){
+    if (OPTION_P) {
+        int i;
+        for (i = 0; i < process_set.size(); i++) {
+            printf("PT[%d]: ", i);
+            vector <pte_t> page_table = process_set[i].page_table;
+            int j;
+            for (j = 0; j < page_table.size(); j++) {
+                pte_t pte = page_table[j];
+                if (pte.present) {
+                    printf("%d:", j);
+                    if (pte.referenced) printf("R");
+                    else printf("-");
+                    if (pte.modified) printf("M");
+                    else printf("-");
+                    if (pte.pageout) printf("S ");
+                    else printf("- ");
+                } else {
+                    if (pte.pageout) printf("# ");
+                    else if (j!=page_table.size()-1) printf("* ");
+                    else printf("*");
+                }
+            }
+            printf("\n");
+        }
+
+    }
+    if (OPTION_F) {
+        printf("FT: ");
+        for (size_t i = 0; i < frame_table.size(); i++) {
+            frame_t frame = frame_table[i];
+            if (frame.mapped) {
+                printf("%d:%d ", frame.proc_id, frame.vpage);
+            } else {
+                printf("* ");
+            }
+        }
+        printf("\n");
+    }
+    if (OPTION_S) {
+        for (int i = 0; i < process_set.size(); i++) {
+            Pstats pstats = process_set[i].pstats;
+            printf("PROC[%d]: U=%lu M=%lu I=%lu O=%lu FI=%lu FO=%lu Z=%lu SV=%lu SP=%lu\n",
+                   i,
+                   pstats.unmaps, pstats.maps, pstats.ins, pstats.outs,
+                   pstats.fins, pstats.fouts, pstats.zeros,
+                   pstats.segv, pstats.segprot);
+        }
+        printf("TOTALCOST %lu %lu %lu %llu %lu\n",
+               inst_count, ctx_switches, process_exits, cost, sizeof(pte_t));
+    }
+}
+int main(int argc, char *const argv[]) {
     char *fvalue;
     char *avalue;
     char *ovalue;
-    int index;
     int num_frames;
     int c;
     opterr = 0;
-    while ((c = getopt(argc, argv, "f:a:o:")) != -1)
-    {
-        switch (c)
-        {
-        case 'f':
-            fvalue = optarg;
-            break;
-        case 'a':
-            avalue = optarg;
-            break;
-        case 'o':
-            ovalue = optarg;
-            break;
-        case '?':
-            if (optopt == 's')
-                fprintf(stderr, "Option -%c requires an argument.\n", optopt);
-            else if (isprint(optopt))
-                fprintf(stderr, "Unknown option `-%c'.\n", optopt);
-            else
-                fprintf(stderr,
-                        "Unknown option character `\\x%x'.\n",
-                        optopt);
-            return 1;
-        default:
-            abort();
+
+    while ((c = getopt(argc, argv, "f:a:o:")) != -1) {
+        switch (c) {
+            case 'f':
+                fvalue = optarg;
+                break;
+            case 'a':
+                avalue = optarg;
+                break;
+            case 'o':
+                ovalue = optarg;
+                break;
+            case '?':
+                if (optopt == 's')
+                    fprintf(stderr, "Option -%c requires an argument.\n", optopt);
+                else if (isprint(optopt))
+                    fprintf(stderr, "Unknown option `-%c'.\n", optopt);
+                else
+                    fprintf(stderr,
+                            "Unknown option character `\\x%x'.\n",
+                            optopt);
+                return 1;
+            default:
+                abort();
         }
     }
     argc -= optind;
-    if (argc < 2)
-    {
+    if (argc < 2) {
         fprintf(stderr, "input or rfile is missing`\\x%x'.\n",
                 optopt);
         return 1;
@@ -100,75 +282,82 @@ int main(int argc, char *const argv[])
     argv += optind;
     string input = argv[0];
     FileLoader exec = FileLoader(input);
-    switch (avalue[0])
-    {
-    case 'f':
-    {
-        printf("FIFO pager is used\n");
-        THE_PAGER = new FIFOPager();
-        break;
-    }
-    case 'r':
-        break;
-    case 'c':
-        break;
-    case 'e':
-        break;
-    case 'a':
-        break;
-    case 'w':
-        break;
-    default:
-        fprintf(stderr,
-                "Unknown option character `\\x%x'.\n",
-                avalue[0]);
-        abort();
-    }
-
-    for (char &c : string(ovalue))
-    {
-        switch (c)
-        {
-        case 'O':
-        {
-            OPTION_O = true;
+    sscanf(fvalue, "%d", &num_frames);
+    MAX_FRAMES = num_frames;
+    switch (avalue[0]) {
+        case 'f': {
+            THE_PAGER = new FIFOPager();
             break;
         }
-        case 'P':
-            OPTION_P = true;
+        case 'r':
+        {
+            THE_PAGER = new RandomPager(argv[1]);
             break;
-        case 'F':
-            OPTION_F = true;
+        }
+        case 'c':{
+            THE_PAGER = new CLOCKPager();
             break;
-        case 'S':
-            OPTION_S = true;
+        }
+        case 'e':{
+            THE_PAGER = new ESCPager();
             break;
-        case 'x':
-            OPTION_x = true;
+        }
+        case 'a':{
+            THE_PAGER = new AGING();
             break;
-        case 'y':
-            OPTION_y = true;
+        }
+        case 'w':{
+            THE_PAGER = new Working_Set();
             break;
-        case 'f':
-            OPTION_f = true;
-            break;
-        case 'a':
-            OPTION_a = true;
-            break;
+        }
         default:
             fprintf(stderr,
                     "Unknown option character `\\x%x'.\n",
-                    c);
+                    avalue[0]);
             abort();
+    }
+    for (char &c: string(ovalue)) {
+        switch (c) {
+            case 'O': {
+                OPTION_O = true;
+                break;
+            }
+            case 'P':
+                OPTION_P = true;
+                break;
+            case 'F':
+                OPTION_F = true;
+                break;
+            case 'S':
+                OPTION_S = true;
+                break;
+            case 'x':
+                OPTION_x = true;
+                break;
+            case 'y':
+                OPTION_y = true;
+                break;
+            case 'f':
+                OPTION_f = true;
+                break;
+            case 'a':
+                OPTION_a = true;
+                break;
+            default:
+                fprintf(stderr,
+                        "Unknown option character `\\x%x'.\n",
+                        c);
+                abort();
         }
     }
-    sscanf(fvalue + 1, "%d", &num_frames);
+
+    frame_table.resize(MAX_FRAMES);
+    int i;
+    for (i = 0; i < MAX_FRAMES; i++) {
+        frame_free_list.push(i);
+    }
     simulation(exec);
     delete (THE_PAGER);
-    if (strcmp(ovalue, "oOPFS"))
-    {
-        printf("OPFS\n");
-    }
-
+    display();
     return 0;
 }
--- Operating system/Lab/lab3/mmu.h
+++ Operating system/Lab/lab3/mmu.h
@@ -1,17 +1,17 @@
 #ifndef mmu_h
 #define mmu_h
+
 #include <fstream>
 #include <vector>
 #include <string>
 #include <cstring>
 #include <sstream>
+#include <queue>
 
-namespace mmu
-{
+namespace mmu {
     using namespace std;
-    const unsigned int MAX_FRAMES = 128;
     const unsigned int MAX_VPAGES = 64;
-
+    unsigned int MAX_FRAMES;
     bool OPTION_O = true;
     bool OPTION_P = true;
     bool OPTION_F = true;
@@ -20,65 +20,80 @@ namespace mmu
     bool OPTION_y = true;
     bool OPTION_f = true;
     bool OPTION_a = true;
+    unsigned long inst_count = 0;
+    unsigned long ctx_switches = 0, process_exits = 0;
+    unsigned long long cost = 0;
 
-    typedef struct
-    {
-        unsigned num_of_frame : 7;
-        unsigned present : 1;
-        unsigned write_protect : 1;
-        unsigned modified : 1;
-        unsigned referenced : 1;
-        unsigned pageout : 1;
-    } pte_t; // can only be total of 32-bit size and will check on this
+    enum cost_map {
+        LOAD_STORE = 1,
+        CONTEXT_SWITCH = 130,
+        EXIT = 1230,
+        MAPS = 350,
+        UNMAPS = 410,
+        INS = 3200,
+        OUTS = 2750,
+        FINS = 2350,
+        FOUTS = 2800,
+        ZEROS = 150,
+        SEGV = 440,
+        SEGPROT = 410
+    };
 
-    typedef struct
-    {
-        unsigned int proc_id : 4;
-        unsigned int vpage : 6;
-        unsigned int mapped : 1;
-        unsigned int age;
+    struct Pstats {
+        unsigned long  unmaps = 0;
+        unsigned long  maps = 0;
+        unsigned long  ins = 0;
+        unsigned long  outs = 0;
+        unsigned long  fins = 0;
+        unsigned long  fouts = 0;
+        unsigned long  zeros = 0;
+        unsigned long  segv = 0;
+        unsigned long  segprot = 0;
+    };
+    typedef struct {
+        unsigned int frame_id: 7;
+        unsigned int proc_id: 4;
+        unsigned int vpage: 6;
+        unsigned int mapped:1;
+        unsigned int age=0;
     } frame_t;
 
-    frame_t frame_table[MAX_FRAMES];
+    typedef struct {
+        unsigned present: 1;
+        unsigned write_protect: 1;
+        unsigned modified: 1;
+        unsigned referenced: 1;
+        unsigned pageout: 1;
+        unsigned frame: 7;
+        unsigned file_mapped: 1;
+    } pte_t; // can only be total of 32-bit size and will check on this
 
-    typedef struct
-    {
-        unsigned int start_vpage : 6;
-        unsigned int end_vpage : 6;
-        unsigned int write_protect : 1;
-        unsigned int file_mapped : 1;
-    } VMA_t;
+    vector <frame_t> frame_table;
 
-    typedef struct
-    {
-        pte_t page_table[MAX_VPAGES];
-        vector<VMA_t> VMAs;
-    } process_t;
+    typedef struct {
+        unsigned int start_vpage: 6;
+        unsigned int end_vpage: 6;
+        unsigned int write_protect: 1;
+        unsigned int file_mapped: 1;
+    } vma_t;
 
-    vector<process_t> process_set;
+    struct process_t {
+        int pid;
+        vector <pte_t> page_table;
+        vector <vma_t> vmas;
+        Pstats pstats;
+    };
 
-    vector<int> get_randfile(string path)
-    {
-        ifstream a_file(path);
-        vector<int> randvals;
-        int item;
-        while (a_file >> item)
-        {
-            randvals.push_back(item);
-        }
-        return randvals;
-    }
+    vector <process_t> process_set;
+    queue<unsigned int> frame_free_list;
 
-    class FileLoader
-    {
+    class FileLoader {
     private:
         ifstream input;
         istringstream iss;
-        bool get_valid_line()
-        {
+        bool get_valid_line() {
             string line;
-            while (getline(input, line))
-            {
+            while (getline(input, line)) {
                 if (line[0] == '#')
                     continue;
                 iss.clear();
@@ -89,57 +104,188 @@ namespace mmu
         }
 
     public:
-        FileLoader(string file) : input(file)
-        {
+        FileLoader(string file) : input(file) {
             get_valid_line();
             int num_process;
             iss >> num_process;
-            while (num_process--)
-            {
+            while (num_process--) {
                 process_t proc;
                 get_valid_line();
                 int num_vma;
                 iss >> num_vma;
-                proc.VMAs.resize(num_vma);
-                while (num_vma--)
-                {
+                while (num_vma--) {
                     get_valid_line();
                     unsigned int start_vpage, end_vpage;
                     bool write_protected, file_mapped;
                     iss >> start_vpage >> end_vpage >> write_protected >> file_mapped;
-                    VMA_t VMA{start_vpage, end_vpage, write_protected, file_mapped};
-                    proc.VMAs.push_back(VMA);
+                    vma_t vma{start_vpage, end_vpage, write_protected, file_mapped};
+                    proc.vmas.push_back(vma);
                 }
+                proc.page_table.resize(MAX_VPAGES);
                 process_set.push_back(proc);
             }
         }
-        bool get_next_instruction(char &operation, unsigned int &vpage)
-        {
-            if (get_valid_line())
-            {
+
+        bool get_next_instruction(char &operation, unsigned int &vpage) {
+            if (get_valid_line()) {
                 iss >> operation >> vpage;
                 return true;
             }
-
             return false;
         }
     };
-    class Pager
-    {
+
+    class Pager {
     public:
-        virtual frame_t *select_victim_frame() = 0; // virtual base class
+        virtual unsigned int select_victim_frame() = 0; // virtual base class
+        virtual void set_age(unsigned int frame){}; // virtual base class
     };
 
-    class FIFOPager : public Pager
-    {
-    private:
-        unsigned int hand;
 
+
+    class FIFOPager : public Pager {
+    private:
+        unsigned int hand=0;
     public:
-        virtual frame_t *select_victim_frame()
-        {
+        virtual unsigned int select_victim_frame() {
             hand %= MAX_FRAMES;
-            return &frame_table[hand++];
+            return hand++;
+        }
+    };
+    class RandomPager: public Pager {
+    private:
+        vector<int> randvals;
+        int ofs = 1;
+        vector<int> get_randfile(string path) {
+            ifstream a_file(path);
+            vector<int> randvals;
+            int item;
+            while (a_file >> item) {
+                randvals.push_back(item);
+            }
+            return randvals;
+        }
+        unsigned int myrandom(unsigned int bound)
+        {
+            if (ofs == randvals.size()) ofs = 1;
+            return randvals[ofs++] % bound;
+        }
+    public:
+        RandomPager(string rfile): randvals(get_randfile(rfile)){}
+        unsigned int select_victim_frame(){
+            return myrandom(MAX_FRAMES);
+        }
+    };
+
+    class CLOCKPager: public Pager{
+    private:
+        unsigned hand = 0;
+    public:
+        unsigned int select_victim_frame(){
+            while(true){
+                frame_t frame = frame_table.at(hand);
+                pte_t& pre_pte = process_set.at(frame.proc_id).page_table.at(frame.vpage);
+                if (pre_pte.referenced){
+                    pre_pte.referenced= false;
+                    hand = (hand+1)%MAX_FRAMES;
+                }else{
+                    unsigned result = hand;
+                    hand = (hand+1)%MAX_FRAMES;
+                    return result;
+                }
+            }
+        }
+    };
+    class ESCPager: public Pager{
+    private:
+        const unsigned int PERIODIC_TIME = 50;
+        unsigned long last_time = 0;
+        unsigned int hand=0;
+    public:
+        unsigned int select_victim_frame(){
+            int classes[4]={-1,-1,-1,-1};
+            int lowest= 4;
+            int start = hand;
+            bool reset = false;
+            if (inst_count-last_time>=PERIODIC_TIME) reset = true;
+            while (true){
+                frame_t frame = frame_table.at(hand);
+                pte_t& pre_pte = process_set.at(frame.proc_id).page_table.at(frame.vpage);
+                int class_ = pre_pte.modified+2*pre_pte.referenced;
+                lowest = min(lowest,class_);
+                if (classes[class_]==-1) classes[class_] = hand;
+                if (class_==0&&!reset) break;
+                if (reset) pre_pte.referenced= false;
+                hand = (hand+1) % MAX_FRAMES;
+                if (hand==start) break;
+            }
+            if (reset) last_time = inst_count;
+            hand = (classes[lowest]+1)%MAX_FRAMES;
+            return classes[lowest];
+        }
+    };
+    class AGING: public Pager{
+    private:
+        unsigned int hand = 0;
+    public:
+        void set_age(unsigned int frame){
+            frame_table[frame].age = 0;
+        }
+        unsigned int select_victim_frame(){
+            unsigned int start = hand;
+            unsigned int lowest = hand;
+            while (true){
+                frame_t& frame = frame_table.at(hand);
+                pte_t& pre_pte = process_set.at(frame.proc_id).page_table.at(frame.vpage);
+                frame.age>>=1;
+                if (pre_pte.referenced){
+                    frame.age |= 0x80000000;
+                    pre_pte.referenced = false;
+                }
+                if (frame.age<frame_table.at(lowest).age){
+                    lowest = hand;
+                }
+                hand = (hand+1)%MAX_FRAMES;
+                if (hand==start) break;
+            }
+            hand = (lowest+1)%MAX_FRAMES;
+            return lowest;
+        }
+    };
+    class Working_Set: public Pager{
+    private:
+        const unsigned int TAU = 49;
+        unsigned int hand = 0;
+        vector<unsigned int> last_time;
+    public:
+        Working_Set(){
+            last_time.resize(MAX_FRAMES);
+        };
+        void set_age(unsigned int frame){
+            last_time[frame] = inst_count;
+        }
+        unsigned int select_victim_frame(){
+            unsigned int start = hand;
+            unsigned int lowest = hand;
+            while (true){
+                frame_t& frame = frame_table.at(hand);
+                pte_t& pre_pte = process_set.at(frame.proc_id).page_table.at(frame.vpage);
+                if (pre_pte.referenced){
+                    last_time[hand] = inst_count;
+                    pre_pte.referenced = false;
+                }
+                if (last_time[hand]+TAU<inst_count){
+                    lowest = hand;
+                    break;
+                }
+                if (last_time[hand]<last_time[lowest]){
+                    lowest = hand;
+                }
+                hand = (hand+1)%MAX_FRAMES;
+                if (hand==start) break;
+            }
+            hand = (lowest+1)%MAX_FRAMES;
+            return lowest;
         }
     };
 }
